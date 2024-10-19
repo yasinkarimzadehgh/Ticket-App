9ed7c68d6123748d16f2c838a0dfde6e9f2036c0
Yasin Karimzadeh <yasinkarimzadehgh@gmail.com>
2024-10-19 18:55:52 +0330

    styling

---
+++ components/ToggleMode.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import React from "react";
+import { useState, useEffect } from "react";
+import { useTheme } from "next-themes";
+import { Moon, Sun } from "lucide-react";
+import { Button } from "./ui/button";
+
+function ToggleMode() {
+  const { theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return <Button variant="outline" size="icon" disabled></Button>;
+  }
+
+  const dark = theme == "dark";
+
+  return (
+    <Button
+      variant="outline"
+      size="icon"
+      onClick={() => setTheme(`${dark ? "light" : "dark"}`)}
+    >
+      {dark ? (
+        <Sun className="hover: cursor-pointer hover:text-primary" />
+      ) : (
+        <Moon className="hover: cursor-pointer hover:text-primary" />
+      )}
+    </Button>
+  );
+}
+
+export default ToggleMode;
