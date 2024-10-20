f8e44c7656700434de54d8b0b00e177dade7549b
Yasin Karimzadeh <yasinkarimzadehgh@gmail.com>
2024-10-20 18:17:51 +0330

    modified

--- app/tickets/page.tsx
+++ app/tickets/page.tsx
@@ -1,6 +1,11 @@
 import React from "react";
 
-const Tickets = () => {
+import prisma from "@/prisma/db";
+
+const Tickets = async () => {
+  const tickets = await prisma.ticket.findMany();
+  console.log(tickets);
+
   return <div>Tickets</div>;
 };
 
